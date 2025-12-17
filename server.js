a7c4c55601ef47cac104c40619e179873cf29df3
Ami <ami.dorsey@me.com>
2025-12-16 22:41:28 -0500

    css

--- server.js
+++ server.js
@@ -70,8 +70,8 @@ app.use((req, res, next) => {
 // GET /
 app.get("/", async (req, res) => {
   if(req.session.user) {
-    res.redirect("/tracks");
-  } else {
+  //   res.redirect("/hub");
+  // } else {
   res.render("index.ejs");
   }
 });
